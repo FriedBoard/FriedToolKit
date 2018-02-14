0fe2797ac496021987feb0676e802c16b15e99b0
Unknown <leroy.vandersteenhoven@student.hu.nl>
2018-02-14 10:05:11 +0100

    Initial commit

---
+++ Powershell/Active Directory related/Install AD forest or domain.ps1
@@ -0,0 +1,31 @@
+﻿#Install the domain or forest
+#Set to true if this is the first domain controller in the forest. 
+#Else set it to false to add a domain controller to the forest
+
+$installForest = $true
+
+#Set installation parameters
+$domainName = 'bmc.local' 
+$installDns = $true
+$domainNetbios = 'bmc'
+
+if($installDns -eq $true){
+    if($installForest -eq $true){
+            write-host('Installing forest ' + $domainName + ' with dns now.' )
+            Install-ADDSForest -DomainName $domainName -InstallDns -DomainNetbiosName $domainNetbios -Confirm:$false
+        }
+        else{
+            write-host('Adding this computer as domain controller to ' + $domainName + ' with dns now.' )
+            Install-ADDSDomainController -DomainName $domainName  -InstallDns -Confirm:$false
+    }
+}
+else{
+    if($installForest -eq $true){
+            write-host('Installing forest ' + $domainName + ' without dns now.' )
+            Install-ADDSForest -DomainName $domainName -DomainNetbiosName $domainNetbios -Confirm:$false
+        }
+        else{
+            write-host('Adding this computer as domain controller to ' + $domainName + ' without dns now.' )
+            Install-ADDSDomainController -DomainName $domainName -Confirm:$false
+    }
+}
---
+++ Powershell/Active Directory related/Install AD-domain-services.ps1
@@ -0,0 +1,26 @@
+﻿#Install the AD-domain-services feature
+
+#true or false
+$managementTools = $true
+
+#Check if ad-domain-services role is installed
+$role = Get-WindowsFeature -Name ad-domain-services
+$rolePresent = $role.Installed
+
+#Start by installing the role
+if($rolePresent -eq $true){
+    #If ad-domain-services is already installed do nothing
+    write-host('Active Directory role is already installed. Proceeding with domain controller installation.')
+}
+
+else{
+    #Else install the service with or without management tools specified
+    if($managementTools -eq $true){
+        Install-WindowsFeature ad-domain-services -IncludeManagementTools 
+    }
+    else{
+        Install-WindowsFeature ad-domain-services
+    }
+}
+
+Restart-Computer
