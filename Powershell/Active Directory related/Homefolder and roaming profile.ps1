b676c0927a2961a1c84e1d5c77f3356bfd2c6233
FriedBoard <31770146+FriedBoard@users.noreply.github.com>
2018-03-10 13:18:55 +0100

    Add files via upload

---
+++ Powershell/Active Directory related/Homefolder and roaming profile.ps1
@@ -0,0 +1,10 @@
+﻿Get-ADUser -Filter * | Foreach-Object{
+
+    $sam = $_.SamAccountName
+
+    #Home folder requires the SAM to be inserted whereas %USERNAME% does work for profilepath 
+    Set-ADuser -Identity $_ -HomeDrive "H:" -HomeDirectory "\\server\folder\$sam" -ProfilePath '\\server\foldder\%USERNAME%'
+    
+    #The home folder has to be made manually unlike the profile
+    md "\\server\folder\$sam"
+}
